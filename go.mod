74603bc665c54cfa41a9a28e57d479cf9fc2debf
Laura Martin <laura@surminus.com>
2022-12-31 17:02:38 +0000

    Update to v0.3.0

--- go.mod
+++ go.mod
@@ -2,26 +2,23 @@ module github.com/surminus/myduct
 
 go 1.19
 
-require github.com/surminus/viaduct v0.2.4
+require github.com/surminus/viaduct v0.2.5-0.20221231165307-8f433027268a
 
 require (
-	github.com/Microsoft/go-winio v0.6.0 // indirect
-	github.com/emirpasic/gods v1.18.1 // indirect
+	github.com/emirpasic/gods v1.12.0 // indirect
 	github.com/fatih/color v1.13.0 // indirect
 	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
-	github.com/kevinburke/ssh_config v1.2.0 // indirect
+	github.com/kevinburke/ssh_config v0.0.0-20190725054713-01f96b0aa0cd // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
-	github.com/sergi/go-diff v1.2.0 // indirect
+	github.com/sergi/go-diff v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/src-d/gcfg v1.4.0 // indirect
-	github.com/xanzy/ssh-agent v0.3.2 // indirect
-	golang.org/x/crypto v0.0.0-20220926161630-eccd6366d1be // indirect
-	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
-	golang.org/x/net v0.0.0-20220926192436-02166a98028e // indirect
+	github.com/xanzy/ssh-agent v0.2.1 // indirect
+	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4 // indirect
+	golang.org/x/net v0.0.0-20190724013045-ca1201d0de80 // indirect
 	golang.org/x/sys v0.0.0-20220926163933-8cfa568d3c25 // indirect
-	golang.org/x/tools v0.1.12 // indirect
 	gopkg.in/src-d/go-billy.v4 v4.3.2 // indirect
 	gopkg.in/src-d/go-git.v4 v4.13.1 // indirect
 	gopkg.in/warnings.v0 v0.1.2 // indirect
--- main.go
+++ main.go
@@ -5,7 +5,8 @@ import (
 	"log"
 	"path/filepath"
 
-	v "github.com/surminus/viaduct"
+	"github.com/surminus/viaduct"
+	"github.com/surminus/viaduct/resources"
 )
 
 const (
@@ -67,25 +68,24 @@ var ubuntuPackages = []string{
 	"xkcdpass",
 }
 
-var r = v.New()
+var r = viaduct.New()
 
 func main() {
-	if v.Attribute.User.Username != "root" {
+	if viaduct.Attribute.User.Username != "root" {
 		log.Fatal("Must run as root")
 	}
 
-	v.Attribute.SetUser("laura")
+	viaduct.Attribute.SetUser("laura")
 
-	r.Create(v.Directory{Path: filepath.Join(v.Attribute.User.HomeDir, "bin")})
+	r.Add(&resources.Directory{Path: filepath.Join(viaduct.Attribute.User.HomeDir, "bin")})
 
-	if v.Attribute.Platform.IDLike == "arch" {
-		r.WithLock(r.Run(v.E("sudo pacman -Syy --needed")))
+	if viaduct.Attribute.Platform.IDLike == "arch" {
+		r.WithLock(r.Add(resources.Exec("sudo pacman -Syy --needed")))
 	}
 
 	zsh()
 	vim()
 	dotfiles()
-	runtimeEnvs()
 	tools()
 	tmux()
 	asdf()
@@ -93,25 +93,24 @@ func main() {
 	slack()
 	nodejs()
 
-	r.Start()
+	r.Run()
 }
 
 func zsh() {
-	r.Create(v.P("zsh"))
-	zsh := r.Create(v.Git{Path: "~/.oh-my-zsh", URL: "https://github.com/ohmyzsh/ohmyzsh.git"})
-	r.Create(v.Git{Path: "~/.oh-my-zsh/custom/plugins/zsh-autosuggestions", URL: "https://github.com/zsh-users/zsh-autosuggestions"}, zsh)
+	r.Add(resources.Pkg("zsh"))
+	zsh := r.Add(&resources.Git{Path: "~/.oh-my-zsh", URL: "https://github.com/ohmyzsh/ohmyzsh.git", Reference: "refs/heads/master"})
+	r.Add(&resources.Git{Path: "~/.oh-my-zsh/custom/plugins/zsh-autosuggestions", URL: "https://github.com/zsh-users/zsh-autosuggestions", Reference: "refs/heads/master"}, zsh)
 }
 
 func vim() {
-	r.Create(v.D("~/.vim/swapfiles"))
+	r.Add(resources.Dir("~/.vim/swapfiles"))
 }
 
 func dotfiles() {
-	repo := r.Create(v.Git{
-		Path:   "~/.dotfiles",
-		URL:    "git@github.com:surminus/dotfiles.git",
-		Ensure: true,
-	})
+	repo := r.Add(resources.Repo(
+		"~/.dotfiles",
+		"git@github.com:surminus/dotfiles.git",
+	))
 
 	files := []string{
 		"colordiffrc",
@@ -127,90 +126,72 @@ func dotfiles() {
 	}
 
 	for _, file := range files {
-		r.Create(v.Link{
+		r.Add(&resources.Link{
 			Path:   "~/." + file,
 			Source: filepath.Join("~/.dotfiles", file),
 		}, repo)
 	}
 
-	r.Create(v.Link{Path: "~/.oh-my-zsh/custom/themes/surminus.zsh-theme", Source: "~/.dotfiles/surminus.zsh-theme"}, repo)
+	r.Add(&resources.Link{Path: "~/.oh-my-zsh/custom/themes/surminus.zsh-theme", Source: "~/.dotfiles/surminus.zsh-theme"}, repo)
 
 	// Add terminator configuration
-	termdir := r.Create(v.Directory{Path: "~/.config/terminator"}, repo)
+	termdir := r.Add(&resources.Directory{Path: "~/.config/terminator"}, repo)
 
-	if v.Attribute.Platform.ID == "manjaro" {
-		r.Create(v.Link{Path: "~/.config/terminator/config", Source: "~/.dotfiles/terminator.manjaro"}, repo, termdir)
+	if viaduct.Attribute.Platform.ID == "manjaro" {
+		r.Add(&resources.Link{Path: "~/.config/terminator/config", Source: "~/.dotfiles/terminator.manjaro"}, repo, termdir)
 	}
 
-	if v.IsUbuntu() {
-		if v.Attribute.Hostname == "laura-hub" {
-			r.Create(v.Link{Path: "~/.config/terminator/config", Source: "~/.dotfiles/terminator.desktop"}, repo, termdir)
+	if viaduct.IsUbuntu() {
+		if viaduct.Attribute.Hostname == "laura-hub" {
+			r.Add(&resources.Link{Path: "~/.config/terminator/config", Source: "~/.dotfiles/terminator.desktop"}, repo, termdir)
 		} else {
-			r.Create(v.Link{Path: "~/.config/terminator/config", Source: "~/.dotfiles/terminator.laptop"}, repo, termdir)
+			r.Add(&resources.Link{Path: "~/.config/terminator/config", Source: "~/.dotfiles/terminator.laptop"}, repo, termdir)
 		}
 	}
 
 	// Ensure CoC is set up correctly
-	vim := r.Create(v.Directory{Path: "~/.vim"})
-	r.Create(v.Link{Path: "~/.vim/coc-settings.json", Source: "~/.dotfiles/coc-settings.json"}, repo, vim)
-}
-
-func runtimeEnvs() {
-	envs := map[string]string{
-		"https://github.com/kamatama41/tfenv.git": "~/.tfenv",
-		"https://github.com/pyenv/pyenv.git":      "~/.pyenv",
-		"https://github.com/rbenv/rbenv.git":      "~/.rbenv",
-		"https://github.com/syndbg/goenv.git":     "~/.goenv",
-	}
-
-	for url, path := range envs {
-		r.Delete(v.Git{
-			Path:      path,
-			URL:       url,
-			Reference: "refs/heads/master",
-			Ensure:    true,
-		})
-	}
+	vim := r.Add(&resources.Directory{Path: "~/.vim"})
+	r.Add(&resources.Link{Path: "~/.vim/coc-settings.json", Source: "~/.dotfiles/coc-settings.json"}, repo, vim)
 }
 
 func tools() {
-	r.Create(v.Git{Path: "~/.fzf", URL: "https://github.com/junegunn/fzf.git"})
+	r.Add(&resources.Git{Path: "~/.fzf", URL: "https://github.com/junegunn/fzf.git"})
 
-	var vim, git *v.Resource
-	if v.IsUbuntu() {
+	var vim, git *viaduct.Resource
+	if viaduct.IsUbuntu() {
 		// vim ppa
-		vim = r.Create(v.Apt{
+		vim = r.Add(&resources.Apt{
 			Name: "vim",
 			URI:  "https://ppa.launchpadcontent.net/jonathonf/vim/ubuntu",
 		})
 
-		git = r.Create(v.Apt{
+		git = r.Add(&resources.Apt{
 			Name: "git",
 			URI:  "https://ppa.launchpadcontent.net/git-core/ppa/ubuntu",
 		})
 	}
 
 	var pkgs []string
-	switch v.Attribute.Platform.ID {
+	switch viaduct.Attribute.Platform.ID {
 	case "manjaro":
 		pkgs = archPackages
-		r.Create(v.Ps(pkgs...))
+		r.Add(resources.Pkgs(pkgs...))
 	default:
 		pkgs = ubuntuPackages
-		r.Create(v.Ps(pkgs...), r.Update(v.Apt{}), vim, git)
+		r.Add(resources.Pkgs(pkgs...), vim, git)
 	}
 
-	if v.IsUbuntu() {
+	if viaduct.IsUbuntu() {
 		// Install delta
 		deltaSource := fmt.Sprintf("https://github.com/dandavison/delta/releases/download/%s/git-delta_%s_amd64.deb", deltaVersion, deltaVersion)
-		deltaPkg := filepath.Join(v.Attribute.TmpDir, "delta.deb")
+		deltaPkg := filepath.Join(viaduct.Attribute.TmpDir, "delta.deb")
 
-		delta := r.Run(v.Execute{
+		delta := r.Add(&resources.Execute{
 			Command: fmt.Sprintf("wget -q %s -O %s", deltaSource, deltaPkg),
 			Unless:  "dpkg -l | grep -q git-delta",
 		})
 
-		r.Run(v.Execute{
+		r.Add(&resources.Execute{
 			Command: "sudo dpkg -i " + deltaPkg,
 			Unless:  "dpkg -l | grep -q git-delta",
 		}, delta)
@@ -218,7 +199,7 @@ func tools() {
 }
 
 func tmux() {
-	r.Create(v.Git{
+	r.Add(&resources.Git{
 		Path:      "~/.tmux/plugins/tpm",
 		URL:       "https://github.com/tmux-plugins/tpm",
 		Reference: "refs/heads/master",
@@ -227,16 +208,16 @@ func tmux() {
 }
 
 func slack() {
-	if v.IsUbuntu() {
+	if viaduct.IsUbuntu() {
 		slackSource := fmt.Sprintf("https://downloads.slack-edge.com/releases/linux/%s/prod/x64/slack-desktop-%s-amd64.deb", slackVersion, slackVersion)
-		slackPkg := filepath.Join(v.Attribute.TmpDir, "slack.deb")
+		slackPkg := filepath.Join(viaduct.Attribute.TmpDir, "slack.deb")
 
-		slack := r.Run(v.Execute{
+		slack := r.Add(&resources.Execute{
 			Command: fmt.Sprintf("wget -q %s -O %s", slackSource, slackPkg),
 			Unless:  "dpkg -l | grep -q slack-desktop",
 		})
 
-		r.Run(v.Execute{
+		r.Add(&resources.Execute{
 			Command: "sudo dpkg -i " + slackPkg,
 			Unless:  "dpkg -l | grep -q slack-desktop",
 		}, slack)
@@ -244,13 +225,13 @@ func slack() {
 }
 
 func asdf() {
-	repo := r.Create(v.Git{
+	repo := r.Add(&resources.Git{
 		Path:      "~/.asdf",
 		URL:       "https://github.com/asdf-vm/asdf",
 		Reference: "refs/tags/v0.10.2",
 	})
 
-	dir := r.Create(v.Directory{Path: "~/.asdf/plugins"}, repo)
+	dir := r.Add(&resources.Directory{Path: "~/.asdf/plugins"}, repo)
 
 	for plugin, url := range map[string]string{
 		"golang": "https://github.com/kennyp/asdf-golang",
@@ -258,7 +239,7 @@ func asdf() {
 		"python": "https://github.com/danhper/asdf-python",
 		"ruby":   "https://github.com/asdf-vm/asdf-ruby",
 	} {
-		r.Create(v.Git{
+		r.Add(&resources.Git{
 			Path:      fmt.Sprintf("~/.asdf/plugins/%s", plugin),
 			URL:       url,
 			Reference: "refs/heads/master",
@@ -268,33 +249,33 @@ func asdf() {
 }
 
 func docker() {
-	if v.IsUbuntu() {
-		apt := r.Create(v.Apt{
+	if viaduct.IsUbuntu() {
+		apt := r.Add(&resources.Apt{
 			Name:       "docker",
 			URI:        "https://download.docker.com/linux/ubuntu",
-			Parameters: map[string]string{"arch": v.Attribute.Arch},
+			Parameters: map[string]string{"arch": viaduct.Attribute.Arch},
 			Source:     "stable",
 		})
 
-		install := r.Create(v.P("docker-ce"), r.Update(v.Apt{}, apt))
+		install := r.Add(resources.Pkg("docker-ce"), r.Add(resources.AptUpdate(), apt))
 
 		// We need to add a User resource here to manage users, so we can
 		// add the docker group to the user
-		r.Run(v.Execute{
-			Command: fmt.Sprintf("usermod -G docker %s", v.Attribute.User.Username),
-			Unless:  fmt.Sprintf("grep %s /etc/group | grep -q docker", v.Attribute.User.Username),
+		r.Add(&resources.Execute{
+			Command: fmt.Sprintf("usermod -G docker %s", viaduct.Attribute.User.Username),
+			Unless:  fmt.Sprintf("grep %s /etc/group | grep -q docker", viaduct.Attribute.User.Username),
 		}, install)
 	}
 }
 
 func nodejs() {
-	if v.IsUbuntu() {
-		key := r.Run(v.Execute{
+	if viaduct.IsUbuntu() {
+		key := r.Add(&resources.Execute{
 			Command: "curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | gpg --dearmor | sudo tee /usr/share/keyrings/nodesource.gpg >/dev/null",
 			Unless:  "dpkg -l | grep -q nodejs",
 		})
 
-		apt := r.Create(v.Apt{
+		apt := r.Add(&resources.Apt{
 			Name: "nodesource",
 			URI:  "https://deb.nodesource.com/node_18.x",
 			Parameters: map[string]string{
@@ -302,7 +283,7 @@ func nodejs() {
 			},
 		}, key)
 
-		update := r.Update(v.Apt{}, apt)
-		r.Create(v.P("nodejs"), update)
+		update := r.Add(resources.AptUpdate(), apt)
+		r.Add(resources.Pkg("nodejs"), update)
 	}
 }
